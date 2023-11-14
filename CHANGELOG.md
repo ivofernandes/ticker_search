bce8059c1a145ee6a5d4e60c9d9755df6be158b2
Ivo Fernandes <ivomuze@gmail.com>
2023-11-14 01:28:38 +0000

    List view to show results

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,6 @@ Initial release
 
 ## 0.0.2
 Improved documentation
+
+## 0.0.3
+Improved performance
--- lib/src/ticker_search.dart
+++ lib/src/ticker_search.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:stock_market_data/stock_market_data.dart';
 import 'package:ticker_search/src/model/ticker_suggestion.dart';
 import 'package:ticker_search/src/ticker_widget_ui.dart';
-import 'package:ticker_search/src/tickers_block.dart';
 
 /// A search delegate that allows users to search for stock tickers.
 ///
@@ -52,88 +51,52 @@ class TickerSearch extends SearchDelegate<List<StockTicker>> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    final searchingWidget = query.isNotEmpty
-        ? TickerWidget(
-            symbol: query.toUpperCase(),
-            onSelection: (StockTicker ticker) => close(context, [ticker]))
-        : Container();
-
-    // Convert the suggestions into a list of TickersBlock widgets.
-    final suggestionWidgets = suggestions
-        .map((suggestion) => TickersBlock(
-              icon: suggestion.icon,
-              title: suggestion.title,
-              tickers: suggestion.companies,
-              query: query,
-              close: close,
-            ))
-        .toList();
-
-    return ListView.builder(
-      itemCount: suggestionWidgets.length + 1, // +1 for the searchingWidget
-      itemBuilder: (context, index) {
-        if (index == 0) {
-          return searchingWidget;
-        }
-        print('index: $index');
-        return suggestionWidgets[index -
-            1]; // Adjust the index by -1 because the searchingWidget is at index 0.
-      },
-    );
-  }
-}
-
-/**
-    Chat gpt suggestion
-
-    @override
-    Widget buildSuggestions(BuildContext context) {
     List<Widget> allItems = [];
 
     if (query.isNotEmpty) {
-    allItems.add(
-    TickerWidget(symbol: query.toUpperCase(), onSelection: (StockTicker ticker) => close(context, [ticker])),
-    );
+      allItems.add(
+        TickerWidget(symbol: query.toUpperCase(), onSelection: (StockTicker ticker) => close(context, [ticker])),
+      );
     }
 
     // Flatten all ticker blocks into a single list of widgets
-    suggestions.forEach((suggestion) {
-    // Add title widget
-    allItems.add(suggestionTitle(suggestion.icon, suggestion.title, context));
+    suggestions.forEach((TickerSuggestion suggestion) {
+      // Add title widget
+      allItems.add(suggestionTitle(suggestion.icon, suggestion.title, context));
 
-    // Filter and add ticker widgets
-    final filteredTickers = suggestion.tickers.entries.where((entry) {
-    final String lowerCaseQuery = query.toLowerCase();
-    return entry.key.toLowerCase().contains(lowerCaseQuery) ||
-    entry.value.toLowerCase().contains(lowerCaseQuery);
-    });
+      // Filter and add ticker widgets
+      final filteredTickers = suggestion.companies.entries.where((entry) {
+        final String lowerCaseQuery = query.toLowerCase();
+        return entry.key.toLowerCase().contains(lowerCaseQuery) || entry.value.toLowerCase().contains(lowerCaseQuery);
+      });
 
-    allItems.addAll(filteredTickers.map((entry) {
-    return TickerWidget(
-    symbol: entry.key,
-    description: entry.value,
-    onSelection: (StockTicker ticker) {
-    close(context, [ticker]);
-    },
-    );
-    }));
+      allItems.addAll(
+        filteredTickers.map(
+          (entry) => TickerWidget(
+            symbol: entry.key,
+            description: entry.value,
+            onSelection: (StockTicker ticker) {
+              close(context, [ticker]);
+            },
+          ),
+        ),
+      );
     });
 
     return ListView.builder(
-    itemCount: allItems.length,
-    itemBuilder: (context, index) {
-    return allItems[index];
-    },
+      itemCount: allItems.length,
+      itemBuilder: (context, index) {
+        return allItems[index];
+      },
     );
-    }
+  }
 
-    // Helper method to create the title widget
-    Widget suggestionTitle(Widget icon, String title, BuildContext context) {
+  // Helper method to create the title widget
+  Widget suggestionTitle(Widget icon, String title, BuildContext context) {
     return ListTile(
-    leading: icon,
-    title: Text(title),
-    // Other properties and callbacks...
+      leading: icon,
+      title: Text(title),
+      // Other properties and callbacks...
     );
-    }
-
- */
+  }
+}
